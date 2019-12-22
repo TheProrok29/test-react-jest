03235883b88bde9ffdac092b68a13a3335e52345
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2019-12-22 14:17:05 +0100

    Prepare helpers to mocking local storage

--- src/helpers.js
+++ src/helpers.js
@@ -1,27 +1,33 @@
-export function initLocalStorage(imageList) {
+export const initLocalStorageCurry = (imageList) => (imageList) => {
   if (localStorage.getItem('imageList') === null) {
     localStorage.setItem('imageList', JSON.stringify(imageList));
   }
 }
 
-export function addComment(imageId, author, text) {
+export const initLocalStorage = initLocalStorageCurry(window.localStorage);
+
+export const addCommentCurry = (localStorage) => (imageId, author, text) => {
   let imageList = JSON.parse(localStorage.getItem('imageList'));
-  imageList[imageId].comments.push(
-    {
-      author,
-      text,
-    }
-  );
+  imageList[imageId].comments.push({
+    author,
+    text,
+  });
 
   localStorage.setItem('imageList', JSON.stringify(imageList));
 }
 
-export function addLike(imageId) {
+export const addComment = addCommentCurry(window.localStorage);
+
+export const addLikeCurry = (localStorage) => (imageId) => {
   let imageList = JSON.parse(localStorage.getItem('imageList'));
   imageList[imageId].likes += 1;
   localStorage.setItem('imageList', JSON.stringify(imageList));
 }
 
-export function getAllImages() {
+export const addLike = addLikeCurry(window.localStorage);
+
+export const getAllImagesCurry = (localStorage) => () => {
   return JSON.parse(localStorage.getItem('imageList'));
 }
+
+export const getAllImages = getAllImagesCurry(window.localStorage);
