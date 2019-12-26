8392e74fe3e677b9c32b607c9b4f37e290e4b1e6
Tomasz Bogacki <tomaszdbogacki@gmail.com>
2019-12-26 12:40:58 +0100

    Add test for gallery using snapshot enzyme shallow

--- src/__tests__/Gallery.spec.js
+++ src/__tests__/Gallery.spec.js
@@ -1,6 +1,11 @@
 import renderer from 'react-test-renderer';
 import React from 'react';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import 'jest-enzyme';
 import Gallery from '../Gallery/Gallery.js';
+
+configure({ adapter: new Adapter() });
 
 describe('Gallery', () => {
     it('should render', () => {
@@ -10,12 +15,28 @@ describe('Gallery', () => {
                 date: '20 July',
                 likes: 13,
                 comments: [],
-            }
-        ]
+            },
+        ];
         const component = renderer
             .create(<Gallery imageList={imageList} />)
             .toJSON();
 
-            expect(component).toMatchSnapshot();
+        expect(component).toMatchSnapshot();
+    });
+
+    it('should render with enzyme', () => {
+        const imageList = [
+            {
+                image: 'photo.jpg',
+                date: '20 July',
+                likes: 13,
+                comments: [],
+            },
+        ];
+
+        const component = shallow(<Gallery imageList={imageList} />);
+        expect(component).toMatchSnapshot();
+
     })
-})
+
+});
