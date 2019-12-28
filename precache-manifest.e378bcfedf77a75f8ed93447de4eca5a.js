a15751f6ab0d065e8a52d15f820169c25eb13be8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-28 14:19:30 +0000

    Deploy TheProrok29/test-react-jest to github.com/TheProrok29/test-react-jest.git:gh-pages

---
+++ precache-manifest.e378bcfedf77a75f8ed93447de4eca5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89486d1b82416a5810c678b2114666ed",
+    "url": "/test-react-jest/index.html"
+  },
+  {
+    "revision": "6f5122e4b0839e89427d",
+    "url": "/test-react-jest/static/css/main.2d24f2e4.chunk.css"
+  },
+  {
+    "revision": "19a320a625fdb9a64872",
+    "url": "/test-react-jest/static/js/2.55039fcf.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test-react-jest/static/js/2.55039fcf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6f5122e4b0839e89427d",
+    "url": "/test-react-jest/static/js/main.1e591b2e.chunk.js"
+  },
+  {
+    "revision": "ada3ab91e58d472a6b05",
+    "url": "/test-react-jest/static/js/runtime-main.3419658c.js"
+  }
+]);
